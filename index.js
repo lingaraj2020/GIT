34e47e968c9d14ddf6b9b3af5cb498225411cdd0
Lingaraj Honasani <lingarajhonasani2020@gmail.com>
2024-09-29 11:28:23 +0530

    changed the some files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const name="raj"
+const name1="raj"
 const age=24
 const dob='01/01/2000'
 
 function sayname(){
-    console.log("my name is "+name);
+    console.log("my name is "+name1);
+}
+
+function isAbove(){
+    return age>=18
 }
